8aee551be900c4c0b10284708b0c7376bd5ee12b
Ihor Babula <igb@kubp.com.ua>
2026-06-29 12:31:33 +0300

    New modul odoo_school_library

---
+++ odoo_school_library/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Oddo School Library',
+    'author': 'Odoo School Student',
+    'website': 'https://odoo.school/',
+    'category': 'Customizations',
+    'version': '19.0.1.0.0',
+    'license': 'OPL-1',
+
+    'depends': [
+        'base'
+    ],
+
+    'external_dependencies': {
+        'python': []
+    },
+
+    'data': [
+    ],
+
+    'demo': [
+    ],
+
+    'images': ['static/description/icon.png'],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
